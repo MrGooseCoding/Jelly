2df83b2801c45ecd2941db7b38ece41137137022
MrGooseCoding <mrgoosepowerful@gmail.com>
2023-03-25 14:08:33 +0100

    Error: Token not working part 2

--- core/static/core/js/login.js
+++ core/static/core/js/login.js
@@ -1,6 +1,22 @@
-$(document).ready(()=>{
-    const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value
-    $('#LoginForm').submit(function(e) {
+$(document).on('ready', ()=>{
+    function getCookie(name) {
+        let cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            const cookies = document.cookie.split(';');
+            for (let i = 0; i < cookies.length; i++) {
+                const cookie = cookies[i].trim();
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+    const csrftoken = getCookie('csrftoken');
+
+    $('#LoginForm').on('submit',function(e) {
         e.preventDefault()
         $.post('/api/account/login/', {username: e.target.username.value, password: e.target.password.value, csrfmiddlewaretoken:csrftoken,}, 
             (data)=>{
