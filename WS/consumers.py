5cfbc686c1dc8d63d49a5b0f5877e3e2de3b2ff5
MrGooseCoding <mrgoosepowerful@gmail.com>
2023-04-04 18:30:29 +0200

    Updated WebSocket(WS/)
    Added multiple room_group_names -> the user can now recieve messages from multiple chats at the same time

--- WS/consumers.py
+++ WS/consumers.py
@@ -7,52 +7,55 @@ from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
 from serializers import MessageSerializer
 
-class ChatConsumer(WebsocketConsumer):
+class Consumer(WebsocketConsumer):
     def connect(self):
 
-        chat_id = self.scope['url_route']['kwargs']['chat_id']
-        self.chat = Chat.objects.get(pk= chat_id)
         token = self.scope['url_route']['kwargs']['token']
         user = Token.objects.get(key=token).user
         self.account = Account.objects.get(user__username = user.username)
-        self.room_group_name = 'chat_' + chat_id 
+        self.room_group_names=[]
 
-        if self.account in self.chat.members.all():
-            self.accept()
+        self.accept()
+
+        for i in self.account.chat_set.all():
+            room_group_name = 'chat_'+str(i.id)
 
             async_to_sync(self.channel_layer.group_add)(
-                self.room_group_name,
+                room_group_name,
                 self.channel_name
             )
-            
-            self.send(text_data= json.dumps({
-                'type': 'connection_established',
-                'message': 'You are now connected'
-            }))
-
-            recent_messages = Message.objects.filter(chat__id = chat_id).order_by('-pub_date')[:10] 
-
-            recent_messages_serialized = MessageSerializer(recent_messages, many=True).data
-
-            self.send(text_data= json.dumps({
-                'type':'recent_messages',
-                'message': recent_messages_serialized[::-1]
-            }))
-
-        else:
-            self.disconnect(404)
+            self.room_group_names.append(room_group_name)
+        
+        self.send(text_data= json.dumps({
+            'type': 'connection_established',
+            'message': 'You are now connected'
+        }))
 
     def receive(self, text_data):
         print(self.account, ': ', text_data)
         text_data_json = json.loads(text_data)
-        msg_object = Message.objects.create(chat= self.chat, author= self.account, content = text_data_json['message'])
+        chat_id = text_data_json['chat_id']
+        chat = Chat.objects.get(pk=chat_id)
+        
+        if text_data_json['type'] == 'recent_messages':
+            if self.account in chat.members.all():
+                recent_messages = Message.objects.filter(chat__id = chat_id).order_by('-pub_date')[:10] 
+                recent_messages_serialized = MessageSerializer(recent_messages, many=True).data
 
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name,
-            { 
-                'type': 'message',
-                'message': MessageSerializer(msg_object, many= False).data
-            })
+                self.send(text_data= json.dumps({
+                    'type':'recent_messages',
+                    'message': recent_messages_serialized[::-1]
+                }))
+        
+        if text_data_json['type'] == 'message':
+            msg_object = Message.objects.create(chat=chat, author= self.account, content = text_data_json['message'])
+
+            async_to_sync(self.channel_layer.group_send)(
+                'chat_'+str(chat_id),
+                { 
+                    'type': 'message',
+                    'message': MessageSerializer(msg_object, many= False).data
+                })
 
     def message(self, event):
         message = event['message']
--- WS/routing.py
+++ WS/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from WS import consumers
 
 websocket_urlpatterns=[
-    re_path(r'ws/(?P<chat_id>[0-9]+)/(?P<token>[0-9a-zA-Z._]+)', consumers.ChatConsumer.as_asgi())
+    re_path(r'ws/(?P<token>[0-9a-zA-Z._]+)', consumers.Consumer.as_asgi())
 ] 
