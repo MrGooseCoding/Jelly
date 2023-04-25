ff78227d6f6c3e474916ebf9a7a0eec4b0dab7fe
MrGooseCoding <mrgoosepowerful@gmail.com>
2023-04-25 12:09:02 +0200

    Account security update

--- API/Account/views.py
+++ API/Account/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from django.views.decorators.csrf import requires_csrf_token
 from core.models import *
 
 from serializers import *
@@ -22,45 +23,48 @@ class AccountViewSet(ModelViewSet):
     @permission_classes([IsAuthenticated])
     def retrieve(self, request):
         try:
-            account = Account.objects.get(user__id=request.user.id)
+            account = Account.objects.get(user=request.user)
             return Response(AccountSerializer(account).data)
         except Exception as e:
             return Response({'status':e})
- 
-    def create(self, request): # Remember to test crsf protection on this view
-        data = request.data['account']
-        user_already_exists = Account.objects.filter(user__username=data['user']['username']).exists()
-        username_regex = r'^[0-9a-zA-Z._+]+$'
+    
+    def create(self, request):
+        try:
+            data = request.data['account']
+            user_already_exists = Account.objects.filter(user__username=data['user']['username']).exists()
+            username_regex = r'^[0-9a-zA-Z._+]+$'
 
-        user_serializer = UserSerializer(data={
-            "username":data['user']['username'].strip(),
-            "first_name":data['user']['username'].strip(),
-            "password":data['user']['password1'],
-            "email":""
-        })
+            user_serializer = UserSerializer(data={
+                "username":data['user']['username'].strip(),
+                "first_name":data['user']['username'].strip(),
+                "password":data['user']['password1'],
+                "email":""
+            })
 
-        if not re.fullmatch(username_regex, data['user']['username'].strip()) or user_already_exists:
-            return Response({'status':'Invalid username'})
+            if not re.fullmatch(username_regex, data['user']['username'].strip()) or user_already_exists:
+                return Response({'status':'Invalid username'})
 
-        if data['user']['password2'] != data['user']['password1']:
-            return Response({'status':'Invalid password2'})
+            if data['user']['password2'] != data['user']['password1']:
+                return Response({'status':'Invalid password2'})
 
-        user_valid = user_serializer.is_valid()
+            user_valid = user_serializer.is_valid()
 
-        if not user_valid:
-            return Response({'status':user_serializer.errors})
+            if not user_valid:
+                return Response({'status':user_serializer.errors})
 
-        user = User(username = data['user']['username'], first_name=data['user']['first_name'])
-        user.save()
-        user.set_password(data['user']['password1'])
-        user.save()
-        account = Account(user = user)
-        account.save()
-        token = Token.objects.create(user = user)
-        response = AccountSerializer(account).data
-        response['token'] = token.key
-        
-        return Response(response)
+            user = User(username = data['user']['username'], first_name=data['user']['first_name'])
+            user.save()
+            user.set_password(data['user']['password1'])
+            user.save()
+            account = Account(user = user)
+            account.save()
+            token = Token.objects.create(user = user)
+            response = AccountSerializer(account).data
+            response['token'] = token.key
+            
+            return Response(response)
+        except Exception as e:
+            return Response({'status':e})
 
     def login(self, request):
         try:
@@ -73,58 +77,69 @@ class AccountViewSet(ModelViewSet):
             return Response({"status": "Invalid Username or Password"})
         except KeyError:
             return Response({'status':'Missing Arguments'})
+        except Exception as e:
+            return Response({'status':e})
 
     @action(detail=True)
     @permission_classes([IsAuthenticated])
     def update_image(self, request):
-        print(request.user)
-        print(request.auth)
-        account = Account.objects.get(user__id=request.user.id)
         try:
-            Image = request.data['image']
-        except KeyError:
-            return Response({'status': 'Request has no resource file attached'})
-        account.image = Image
-        account.save()
-        return Response(AccountSerializer(account).data)
+            print(request.user)
+            print(request.auth)
+            account = Account.objects.get(user__id=request.user.id)
+            try:
+                Image = request.data['image']
+            except KeyError:
+                return Response({'status': 'Request has no resource file attached'})
+            account.image = Image
+            account.save()
+            return Response(AccountSerializer(account).data)
+        except Exception as e:
+            return Response({'status':e})
 
     @action(methods=['POST'], detail=True) 
     def exists(self, request):
-        user_already_exists = Account.objects.filter(user__username=request.data['username']).exists()
-        return Response({'data':user_already_exists})
+        try:
+            user_already_exists = Account.objects.filter(user__username=request.data['username']).exists()
+            return Response({'data':user_already_exists})
+        except Exception as e:
+            return Response({'status':e})
 
     @action(methods=['POST'], detail=True)
     def edit(self, request):
-        """
-            Request data params are:
-                username: str/empty | The user's new username, leave empty to keep the original 
-                first_name: str/empty | The user's new first_name, leave empty to keep the original one
-                description: str/empty | The user's new description, leave empty to keep the original one
-            
-            The request must have a userToken auth param to identify the user
-        """
+        try:
+            """
+                Request data params are:
+                    username: str/empty | The user's new username, leave empty to keep the original 
+                    first_name: str/empty | The user's new first_name, leave empty to keep the original one
+                    description: str/empty | The user's new description, leave empty to keep the original one
+                
+                The request must have a userToken auth param to identify the user
+            """
 
-        account = Account.objects.get(user=request.user)
+            account = Account.objects.get(user=request.user)
 
-        if not request.data['username'].strip() == request.user.username:
-            if User.objects.filter(username=request.data['username'].strip()).exists():
-                return Response({'status':'Username is already taken'})
+            if not request.data['username'].strip() == request.user.username:
+                if User.objects.filter(username=request.data['username'].strip()).exists():
+                    return Response({'status':'Username is already taken'})
 
-        account_serializer = AccountSerializer(data={
-            "description":request.data['description']
-        })
+            account_serializer = AccountSerializer(data={
+                "description":request.data['description']
+            })
 
-        user_serializer = UserSerializer(data={
-            "username":request.data['username'],
-            "first_name":request.data['first_name'],
-            "email":"",
-            "password":"SomeDummyPass"
-        })
+            user_serializer = UserSerializer(data={
+                "username":request.data['username'],
+                "first_name":request.data['first_name'],
+                "email":"",
+                "password":"SomeDummyPass"
+            })
 
-        if not user_serializer.is_valid() or not account_serializer.is_valid():
-            return Response({'account': account_serializer.errors, 'user': user_serializer.errors})
+            if not user_serializer.is_valid() or not account_serializer.is_valid():
+                return Response({'account': account_serializer.errors, 'user': user_serializer.errors})
 
-        print(request.data)
-        account = account_serializer.update(instance=account, validated_data=request.data)
+            print(request.data)
+            account = account_serializer.update(instance=account, validated_data=request.data)
 
-        return Response({'account': AccountSerializer(account).data})
+            return Response({'account': AccountSerializer(account).data})
+        except Exception as e:
+            return Response({'status':e})
